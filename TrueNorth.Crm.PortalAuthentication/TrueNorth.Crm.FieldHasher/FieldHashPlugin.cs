06ee6985dc081a14965a7d0e521c914a09866c0b
Christian Waters <CWaters@trueNorthIT.co.uk>
2017-05-24 13:15:42 +0100

    changed to take unsecure config specifying field names

--- TrueNorth.Crm.PortalAuthentication/TrueNorth.Crm.FieldHasher/FieldHashPlugin.cs
+++ TrueNorth.Crm.PortalAuthentication/TrueNorth.Crm.FieldHasher/FieldHashPlugin.cs
@@ -4,20 +4,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace TrueNorth.Crm.FieldHasher
 {
     public class FieldHashPlugin : IPlugin
     {
+
+        //Unsecure config should be in this format
+        //   <Settings>
+        //     <setting name = "PasswordField" >
+        //         <value>tncore_password</value>
+        //     </setting>
+        //     <setting name = "DontHashField" >
+        //         <value>tncore_donthash</value>
+        //     </setting>
+        //</Settings>
+
+        private string passwordField;
+        private string dontHashField;
+        
+        public FieldHashPlugin(string unsecureconfig, string securconfig)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(unsecureconfig);
+
+            string passwordField = PluginConfiguration.GetConfigDataString(doc, "PasswordField");
+            string dontHashField = PluginConfiguration.GetConfigDataString(doc, "DontHashField");
+
+            this.passwordField = String.IsNullOrEmpty(passwordField) ? "tn_password" : passwordField;
+            this.dontHashField = String.IsNullOrEmpty(dontHashField) ? "tn_donthash" : dontHashField;
+        }
+
         public void Execute(IServiceProvider serviceProvider)
         {
             var context = (IPluginExecutionContext)
                serviceProvider.GetService(typeof(IPluginExecutionContext));
 
             var entity = (Entity)context.InputParameters["Target"];
-            if (entity.Contains("tn_password"))
+            if (entity.Contains(passwordField))
             {
-                if (entity.Attributes.Contains("tn_donthash") && (bool)entity.Attributes["tn_donthash"])
+                if (entity.Attributes.Contains(dontHashField) && (bool)entity.Attributes[dontHashField])
                 {
                     //dont hash it if don't hash is set to Yes
                     //this will be the case when the portal has already hashed a users password reset request for transmission
@@ -25,11 +52,11 @@ namespace TrueNorth.Crm.FieldHasher
                 else
                 {
                     //all other cases, hash the entered password
-                    entity.Attributes["tn_password"] = Hashor.HashPassword((string)entity.Attributes["tn_password"]);
+                    entity.Attributes[passwordField] = Hashor.HashPassword((string)entity.Attributes[passwordField]);
                 }
 
                 //always set dont hash to false afterwards
-                entity.Attributes["tn_donthash"] = false;
+                entity.Attributes[dontHashField] = false;
             }
         }
     }
---
+++ TrueNorth.Crm.PortalAuthentication/TrueNorth.Crm.FieldHasher/PluginConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TrueNorth.Crm.FieldHasher
+{
+    class PluginConfiguration
+    {
+        private static string GetValueNode(XmlDocument doc, string key)
+        {
+            XmlNode node = doc.SelectSingleNode(String.Format("Settings/setting[@name='{0}']", key));
+     
+            if (node != null)
+            {
+                return node.SelectSingleNode("value").InnerText;
+            }
+            return string.Empty;
+        }
+     
+        public static Guid GetConfigDataGuid(XmlDocument doc, string label)
+        {
+            string tempString = GetValueNode(doc, label);
+     
+            if (tempString != string.Empty)
+            {
+                return new Guid(tempString);
+            }
+            return Guid.Empty;
+        }
+     
+        public static bool GetConfigDataBool(XmlDocument doc, string label)
+        {
+            bool retVar;
+     
+            if (bool.TryParse(GetValueNode(doc, label), out retVar))
+            {
+                return retVar;
+            }
+            else
+            {
+                return false;
+            }
+        }
+     
+        public static int GetConfigDataInt(XmlDocument doc, string label)
+        {
+            int retVar;
+     
+            if (int.TryParse(GetValueNode(doc, label), out retVar))
+            {
+                return retVar;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+     
+        public static string GetConfigDataString(XmlDocument doc, string label)
+        {
+            return GetValueNode(doc, label);
+        }
+    }
+}
